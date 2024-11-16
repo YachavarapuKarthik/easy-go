3a747432aa82cd3c3b7271e21d69b220e07e780e
YachavarapuKarthik <karthikyacharapu@gmail.com>
2024-11-16 10:56:06 +0530

    Loops introduced

---
+++ loops.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func loops() {
+	// for {
+	// 	fmt.Println("This is infinite loop")
+	// }
+
+	// This acts like a while loop
+	// i:=0
+
+	// for i<=5{
+	// 	fmt.Println(i)
+	// 	i++
+	// }
+
+	// Standard for loop
+
+	for i:=0 ; i<=5 ; i++{
+		fmt.Println(i)
+	}
+
+}
