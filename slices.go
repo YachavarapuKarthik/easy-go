e8fd6f4e9feb7923a13adbfb122829812c767fae
YachavarapuKarthik <karthikyacharapu@gmail.com>
2024-11-17 14:34:34 +0530

    Updated slices

--- slices.go
+++ slices.go
@@ -3,10 +3,12 @@ package main
 import "fmt"
 
 func slice(){
-	var arr_slice []int
+	//var arr_slice []int
+	arr_slice :=[]int{}
 	for i:=0 ; i < 20 ; i++{
 		arr_slice = append(arr_slice,i+1)
 		fmt.Print(arr_slice[i]," ")
 	}
-
+	fmt.Println("\n",len(arr_slice))
+	fmt.Print(arr_slice[:2])
 }
